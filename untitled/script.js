101de30e8b58b52f97cb82d927cfbe6f14b9fdc8
Ivan <example@gmail.com>
2020-04-24 00:31:19 +0300

    Это наш первый коммит

---
+++ untitled/script.js
@@ -0,0 +1,48 @@
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+var person = {
+    name: age,
+    age: 24,
+    isMarried: false
+};
+
+if (person.isMarried) {
+    console.log("true")
+} else{
+    console.log("false");
+}
+
+
+
+var name = "Max";
+
+var age = 35;
+
+var human= {
+    weight: 67
+}
+
+var person = {
+    name,
+    age,
+    human
+}
+
+var weight = person.human.weight
+
+console.log("weight " + weight);
